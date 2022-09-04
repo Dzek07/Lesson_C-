d833e037688d209fa563e7b0e062f0b73dd543e8
Dzek <dzaek07@gmail.com>
2022-09-04 21:53:30 +0300

    Task_13

---
+++ DZ2/13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string x = Console.ReadLine();
+ThirdNumber(x);
+
+void ThirdNumber(string x)
+{
+    if(x.Length > 2)
+        Console.WriteLine(x[2]);
+    else
+        Console.WriteLine("Нет третьей цифры");
+}
