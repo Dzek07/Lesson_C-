9a41b0fe7d5d62fe1049083382711c41c7c0260b
Dzek <dzaek07@gmail.com>
2022-09-04 21:55:20 +0300

    Task_25

---
+++ DZ4/25/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+int result = Degre(x,y);
+
+Console.WriteLine(result);
+
+
+int Degre(int x, int y)
+{
+    int degre = 1;
+    for(int count = 0; count < y; count++)
+        degre = degre * x;
+    
+    return degre;
+    
+}
