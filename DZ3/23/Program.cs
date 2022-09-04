b47b89b4b3442eecb40b8b1fad88933bb39ee6b3
Dzek <dzaek07@gmail.com>
2022-09-04 21:54:14 +0300

    Task_23

---
+++ DZ3/23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int cub = 0;
+Cube(x,cub,i);
+
+
+void Cube(int x, double cub, int i)
+{
+    while(i < x + 1)
+    {
+        cub = Math.Pow(i, 3);
+        Console.Write(cub + ", ");
+        i++;
+    }
+        
+}
