fbac0962ff0de162b20fc45032c4ac07cb614046
Dzek <dzaek07@gmail.com>
2022-09-04 21:52:44 +0300

    Task_6

---
+++ DZ1/6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int y = x % 2;
+if (y == 0)
+{
+    Console.Write("Четное число: ");
+    Console.WriteLine(x);
+}
+
+    else
+    {
+        Console.Write("Не четное число: ");
+        Console.WriteLine(x);
+    }
+    
