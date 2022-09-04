aa3b7aac0c21c653d607a614d25ae95852afbd12
Dzek <dzaek07@gmail.com>
2022-09-04 21:55:55 +0300

    Task_34

---
+++ DZ5/34/Program.cs
@@ -0,0 +1,34 @@
+﻿void ArrayFilling(int[] array)
+{
+    int size = array.Length;
+
+    for(int index = 0; index < size; index++)
+        array[index] = new Random().Next(100,1000);
+
+}
+
+int EvenNumbers(int[] array)
+{
+    int result = 0;
+    int size = array.Length;
+    for(int index = 0; index < size; index++)
+    {
+        
+
+        if(array[index] % 2 == 0)
+            result = result + 1;
+
+    }
+    return result;
+}
+
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());;
+
+int[] array = new int[size];
+ArrayFilling(array);
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+Console.Write("Колличество четных чисел в массиве " + EvenNumbers(array));
