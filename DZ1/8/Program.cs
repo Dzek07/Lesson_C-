1d5f3a1d68380adb1ee2d1990e1a808690a223b8
Dzek <dzaek07@gmail.com>
2022-09-04 21:53:01 +0300

    Task_8

---
+++ DZ1/8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= x)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i = i + 2;
+}
