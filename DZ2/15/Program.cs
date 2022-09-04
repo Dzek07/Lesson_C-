2fe7c1661b043ea36acd808bcbcf7945e3d491aa
Dzek <dzaek07@gmail.com>
2022-09-04 21:53:37 +0300

    Task_15

---
+++ DZ2/15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Week(x);
+
+void Week(int x)
+{
+    if (x>0 && x<8)
+    {
+        if (x<6)
+            Console.WriteLine("Сегодня не выходной");
+        else
+            Console.WriteLine("Сегодня выходной");
+    }
+    else
+        Console.WriteLine("Введите число от 1 до 7 еще раз");
+    
+}
