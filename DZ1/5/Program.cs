2fe53d237eafdb26dd8c670bb9a2bdea05125c48
Dzek <dzaek07@gmail.com>
2022-09-04 21:52:34 +0300

    Task_5

---
+++ DZ1/5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = x * (-1);
+
+while (i <= x)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i++;
+}
