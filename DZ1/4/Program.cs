f75da2320a6c31a1afcf6c3a3e948334d54ed23b
Dzek <dzaek07@gmail.com>
2022-09-04 21:52:24 +0300

    Task_4

---
+++ DZ1/4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+    if (x > z)
+    {
+        Console.Write("Максимальное число ");
+        Console.WriteLine(x);
+    }
+
+    else
+    {
+        Console.Write("Максимальное число ");
+        Console.WriteLine(z);
+    }
+else if (y > z)
+    {
+        Console.Write("Максимальное число ");
+        Console.WriteLine(y);
+    }
+    else
+    {
+        Console.Write("Максимальное число ");
+        Console.WriteLine(z);
+    }
