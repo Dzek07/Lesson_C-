08815c1af2e4098f0b1b103339f84dca8fb2d96d
Dzek <dzaek07@gmail.com>
2022-09-04 21:55:28 +0300

    Task_27

---
+++ DZ4/27/Program.cs
@@ -0,0 +1,22 @@
+﻿int Sum(string number)
+{
+    int result = 0;
+
+    for(int index = 0; index < number.Length; index++)
+        result += number[index] - '0';
+
+    return result;
+}
+
+
+
+
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+Console.WriteLine(Sum(number));
+
+
+
