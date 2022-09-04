539d9d57e8929197fc29cb03ae158fc7da1c0b90
Dzek <dzaek07@gmail.com>
2022-09-04 21:55:36 +0300

    Task_29

---
+++ DZ4/29/Program.cs
@@ -0,0 +1,13 @@
+﻿void ArrayFilling(int[] array)
+{
+    int size = array.Length;
+
+    for(int index = 0; index < size; index++)
+        array[index] = new Random().Next(1,100);
+
+}
+
+
+int[] array = new int[8];
+ArrayFilling(array);
+Console.Write(string.Join(", ", array));
