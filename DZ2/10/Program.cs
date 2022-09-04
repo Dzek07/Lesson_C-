2ac1551449d2a142f6f000efa8642aba68e0be04
Dzek <dzaek07@gmail.com>
2022-09-04 21:53:20 +0300

    Task_10

---
+++ DZ2/10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write(Second_number(x));
+
+int Second_number(int x)
+{
+    x = x / 10;
+    x = x % 10;
+    return x;
+}
