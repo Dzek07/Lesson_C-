0a98af2ac4461a32e0c86e387be1ada8cc07be3a
Dzek <dzaek07@gmail.com>
2022-09-04 21:56:03 +0300

    Task_36

---
+++ DZ5/36/Program.cs
@@ -0,0 +1,32 @@
+﻿void ArrayFilling(int[] array)
+{
+    int size = array.Length;
+
+    for(int index = 0; index < size; index++)
+        array[index] = new Random().Next(-100,100);
+
+}
+
+int OddSum(int[] array)
+{
+    int result = 0;
+    int size = array.Length;
+    for(int index = 1; index < size; index+=2)
+    {
+        
+        result = result + array[index];
+
+    }
+    return result;
+}
+
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());;
+
+int[] array = new int[size];
+ArrayFilling(array);
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+Console.Write("Сумма элементов, стоящих на нечетных позициях " + OddSum(array));
