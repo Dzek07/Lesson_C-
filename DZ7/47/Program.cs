94bd69b48d45f32a2348c17789a928e4a951d72f
Dzek <dzaek07@gmail.com>
2022-09-11 21:18:15 +0300

    Task_47

---
+++ DZ7/47/Program.cs
@@ -0,0 +1,33 @@
+﻿int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+
+void FuelArray(int m, int n, double[,] array)
+{
+    double tmp = 0;
+	for(int i = 0; i<m; i++){
+		for(int j = 0; j<n; j++){
+			array[i,j] =new Random().Next(-10,10);
+            tmp = new Random().Next(0,9);
+            tmp = tmp / 10;
+            array[i,j] = array[i,j] + tmp;
+        }
+	}
+
+	for(int i = 0; i<array.GetLength(0); i++){
+		for(int j = 0; j<array.GetLength(1); j++){
+			Console.Write(array[i,j] + "  ");
+		}
+		Console.WriteLine();
+	}
+}
+
+int m = Read("Введите m ");
+int n = Read("Введите n ");
+double [,] array = new double[m, n];
+FuelArray(m, n, array);
+
+
