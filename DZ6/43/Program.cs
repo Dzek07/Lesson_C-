34d575864b56b3f789d10264a71b6b498e123687
Dzek <dzaek07@gmail.com>
2022-09-11 13:34:24 +0300

    Task_43

---
+++ DZ6/43/Program.cs
@@ -0,0 +1,23 @@
+﻿double Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+
+
+
+double b1 = Read("Введите b1 ");
+double k1 = Read("Введите k1 ");
+double b2 = Read("Введите b2 ");
+double k2 = Read("Введите k2 ");
+
+double x = (b2 - b1)/(k1 - k2);
+double y = 5 * x + 2;
+
+Console.WriteLine("(" + x + ";" + y + ")");
+
+
+
+
+
