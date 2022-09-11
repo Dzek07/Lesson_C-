ed1f4a7f184a8994596eb6058bb0cc3b9f58b432
Dzek <dzaek07@gmail.com>
2022-09-11 13:34:11 +0300

    Task_41

---
+++ DZ6/41/Program.cs
@@ -0,0 +1,38 @@
+﻿int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+void ArrayFilling(int[] array)
+{
+    int size = array.Length;
+
+    for(int index = 0; index < size; index++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+
+int PositiveNumber(int[] array)
+{
+    int size = array.Length;
+    int resault = 0;
+    for(int index = 0; index < size; index++)
+    {
+        if(array[index] > 0)
+            resault ++;
+    }
+    return resault;
+}
+
+
+int size = Read("Сколько будет чисел: ");
+
+int[] array = new int[size];
+ArrayFilling(array);
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+Console.Write(PositiveNumber(array) + " чисел больше 0 ввел пользователь");
