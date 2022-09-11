c345b102403bf2953a7b8186934debdd02598029
Dzek <dzaek07@gmail.com>
2022-09-11 21:18:33 +0300

    Task_52

---
+++ DZ7/52/Program.cs
@@ -0,0 +1,45 @@
+﻿int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+
+void FuelArray(int m, int n, int[,] array)
+{
+    for(int i = 0; i<m; i++){
+		for(int j = 0; j<n; j++)
+        {
+			array[i,j] =new Random().Next(0,10);
+        }
+	}
+
+	for(int i = 0; i<array.GetLength(0); i++)
+    {
+		for(int j = 0; j<array.GetLength(1); j++)
+        {
+			Console.Write(array[i,j] + "  ");
+		}
+		Console.WriteLine();
+	}
+}
+void ElementSearch(int m, int n, int[,] array)
+{
+    for(int j = 0; j<m; j++)
+    {
+		double sum = 0;
+        for(int i = 0; i<n; i++)
+        {
+			sum = sum + array[i,j];
+        }
+    Console.Write((sum/m) + "  ");
+	}
+}
+
+int m = Read("Введите № столбца ");
+int n = Read("Введите № строки ");
+
+int [,] array = new int[m, n];
+FuelArray(m, n, array);
+ElementSearch(m, n, array);
+
