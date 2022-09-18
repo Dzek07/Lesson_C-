3beef524e19a4176f275ef55d1b9e91c6fa22b93
Dzek <dzaek07@gmail.com>
2022-09-18 22:32:35 +0300

    Массив 4х4 спирально заполненный

---
+++ DZ8/62/Program.cs
@@ -0,0 +1,43 @@
+﻿int[,] Spiral(int[,] array,int m, int n)
+{
+    int size = m*n;
+    int num = 1;
+    int i = 0;
+    int j = 0;
+    while(num<size+1)
+    {
+        array[i,j] = num;
+        num++;
+        if(i <= j+1 && i+j < n-1)
+            j++;
+        else if (i<j && i+j >= m-1)
+            i++;
+        else if (i >= j && i+j > n-1)
+            j--;
+        else 
+            i--;
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+		for(int j = 0; j<array.GetLength(1); j++)
+        {
+			Console.Write(array[i,j] + "  ");
+		}
+		Console.WriteLine();
+	}
+}
+
+
+
+int m = 4;
+int n = 4;
+int [,] array = new int[m, n];
+
+Spiral(array,m,n);
+PrintArray(array);
+
