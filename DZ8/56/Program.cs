eb63af6b7c24945a055a35ef11fbb3e980699f9e
Dzek <dzaek07@gmail.com>
2022-09-18 22:24:37 +0300

    Находит строчку с наименьшей суммой элиментов

---
+++ DZ8/56/Program.cs
@@ -0,0 +1,64 @@
+﻿int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+
+void FuelArray(int m, int n, int[,] array)
+{
+    for(int i = 0; i<m; i++){
+		for(int j = 0; j<n; j++)
+        {
+			array[i,j] =new Random().Next(0,10);
+        }
+	}
+
+	for(int i = 0; i<array.GetLength(0); i++)
+    {
+		for(int j = 0; j<array.GetLength(1); j++)
+        {
+			Console.Write(array[i,j] + "  ");
+		}
+		Console.WriteLine();
+	}
+}
+
+
+int MinSumLine(int[,] array)
+{
+    int sum = 0;
+    int minline = 0;
+    int sumline = 0;
+    for(int i =0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+            sumline = sumline + array[i,j];
+        }
+        if(i==0)
+            sum = sumline;
+        if(sumline<sum)
+        {
+            sum = sumline;
+            minline = i+1;
+        }
+        sumline = 0;
+    }
+    return minline;
+
+}
+
+
+int m = Read("Введите Кол-во строк ");
+int n = Read("Введите Кол-во столбцов ");
+
+
+
+
+int [,] array = new int[m, n];
+
+FuelArray(m, n, array);
+Console.WriteLine("-------");
+
+Console.WriteLine("В " + MinSumLine(array) + " строке наименьшая сумма элементов");
