d9804f651c6af9d9d022ca3bada1c5a238d59c35
Dzek <dzaek07@gmail.com>
2022-09-19 21:50:55 +0300

    Находит сумму натуральных числ от M до N через рекурсию

---
+++ DZ9/66/Program.cs
@@ -0,0 +1,22 @@
+﻿int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+int Sum(int num)
+{
+    if (num == 0) 
+        return num;
+ 
+    return (num + Sum(num - 1));
+}
+
+
+int m = Read("Введите M ");
+int n = Read("Введите N ");
+
+if(n>m)
+    Console.WriteLine(Sum(n)-Sum(m-1));
+
+else Console.WriteLine(Sum(m)-Sum(n-1));
