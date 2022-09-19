53251e131a61704b4c5ee5a4c0d497f9f6f8723a
Dzek <dzaek07@gmail.com>
2022-09-19 21:51:44 +0300

    Вычесляет функцию Аккермана через рекурсию

---
+++ DZ9/68/Program.cs
@@ -0,0 +1,22 @@
+﻿int AckermannFun(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return AckermannFun(n - 1, 1);
+    else
+      return AckermannFun(n - 1, AckermannFun(n, m - 1));
+}
+
+int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+int m = Read("Введите M ");
+int n = Read("Введите N ");
+
+Console.WriteLine(AckermannFun(n,m));
+
