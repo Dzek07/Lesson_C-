adb633072c9088f6c3a0cf8916f31f4bbe443b94
Dzek <dzaek07@gmail.com>
2022-09-19 21:49:44 +0300

    Выводит все натуральные числа от M до N через рекурсию

---
+++ DZ9/64/Program.cs
@@ -0,0 +1,20 @@
+﻿int Read(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+string Numbers(int m, int n)
+{
+    if (m == n) 
+        return m.ToString();
+ 
+    return (m + " " + Numbers(m + 1, n));
+}
+
+
+int m = Read("Введите M ");
+int n = Read("Введите N ");
+
+
+Console.WriteLine(Numbers(m,n));
